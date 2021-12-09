540e48dee6f4c92e59d6bff1237948f7b73336b1
Valentin Boettcher <hiro@protagon.space>
2021-12-09 11:34:31 +0100

    scratch that

--- flake.nix
+++ flake.nix
@@ -2,18 +2,18 @@
   outputs = { self }: {
     lib = rec {
       currentDefaultPackage = flake: system: flake.defaultPackage.${system};
-      makeDefaultPackageOverrides =
-        flakes: inputs: system:
-        let
-          body = (builtins.listToAttrs (builtins.map
-          (name: {
-            name = name;
-            value = (currentDefaultPackage flake system);
-          })
-          flakes));
-        in (self: super:
-          body
-        );
+      # makeDefaultPackageOverrides =
+      #   flakes: inputs: system:
+      #   let
+      #     body = (builtins.listToAttrs (builtins.map
+      #     (name: {
+      #       name = name;
+      #       value = (currentDefaultPackage flake system);
+      #     })
+      #     flakes));
+      #   in (self: super:
+      #     body
+      #   );
     };
   };
 }
