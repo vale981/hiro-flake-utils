7c1ede12aefa3a89e2316abe703f958686e69885
Valentin Boettcher <hiro@protagon.space>
2021-12-09 12:05:04 +0100

    fix wrapper

--- flake.nix
+++ flake.nix
@@ -9,63 +9,64 @@
     lib = rec {
       currentDefaultPackage = flake: system: flake.defaultPackage.${system};
       makeDefaultPackageOverrides =
-          (flakes: system:
-        let
-          body = (nixpkgs.lib.attrsets.mapAttrs'
-            (name: flake:
-              {
-              name = nixpkgs.lib.strings.toLower name;
-              value = (currentDefaultPackage flake system);
-              })
-            flakes);
-        in (self: super:
-          body
-        ));
+        (flakes: system:
+          let
+            body = (nixpkgs.lib.attrsets.mapAttrs'
+              (name: flake:
+                {
+                  name = nixpkgs.lib.strings.toLower name;
+                  value = (currentDefaultPackage flake system);
+                })
+              flakes);
+          in
+          (self: super:
+            body
+          ));
 
-      poetry2nixWrapper = pythonInputs: {name, poetryArgs, buildInputs}:
+      poetry2nixWrapper = pythonInputs: { name, poetryArgs ? {}, buildInputs ? _: [] }:
         (flake-utils.lib.eachDefaultSystem (system:
           let
             overlay = nixpkgs.lib.composeManyExtensions [
               poetry2nix.overlay
 
               (final: prev:
-            let overrides = prev.poetry2nix.overrides.withDefaults
-              (makeDefaultPackageOverrides pythonInputs system);
-            in
-            {
-              ${name} = (prev.poetry2nix.mkPoetryApplication {
-                projectDir = ./.;
-                preferWheels = true;
-                overrides = overrides;
-              });
+                let overrides = prev.poetry2nix.overrides.withDefaults
+                  (makeDefaultPackageOverrides pythonInputs system);
+                in
+                {
+                  ${name} = (prev.poetry2nix.mkPoetryApplication {
+                    projectDir = ./.;
+                    preferWheels = true;
+                    overrides = overrides;
+                  });
 
-              "${name}Shell" = (prev.poetry2nix.mkPoetryEnv {
-                projectDir = ./.;
-                overrides = overrides;
-                preferWheels = true;
-                editablePackageSources = {
-                  ${name} = ./${name};
-                };
-              });
-            })
+                  "${name}Shell" = (prev.poetry2nix.mkPoetryEnv {
+                    projectDir = ./.;
+                    overrides = overrides;
+                    preferWheels = true;
+                    editablePackageSources = {
+                      ${name} = ./${name};
+                    };
+                  });
+                })
 
-        ];
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ overlay ];
-          config.allowUnfree = true;
-        };
-      in
-      rec {
-        packages = {
-          ${name} = pkgs.${name};
-        };
+            ];
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [ overlay ];
+              config.allowUnfree = true;
+            };
+          in
+          rec {
+            packages = {
+              ${name} = pkgs.${name};
+            };
 
-        defaultPackage = packages.${name};
-        devShell = pkgs."${name}Shell".env.overrideAttrs (oldAttrs: {
-          buildInputs = (buildInputs.pkgs);
-        });
-      }));
+            defaultPackage = packages.${name};
+            devShell = pkgs."${name}Shell".env.overrideAttrs (oldAttrs: {
+              buildInputs = (buildInputs.pkgs);
+            });
+          }));
     };
   };
 }
