2b569bf289317a64802440a0ea713af136d13b80
Valentin Boettcher <hiro@protagon.space>
2021-12-09 11:13:59 +0100

    add mapDefaultPackageOverrides

--- flake.nix
+++ flake.nix
@@ -1,7 +1,19 @@
 {
   outputs = { self }: {
-    lib = {
+    lib = rec {
       currentDefaultPackage = flake: system: flake.defaultPackage.${system};
+      makeDefaultPackageOverrides =
+        flakes: inputs: system:
+        let
+          body = (builtins.listToAttrs (builtins.mapAttrs
+          (name: _: {
+            name = name;
+            value = (currentDefaultPackage inputs.${name} system);
+          })
+          flakes));
+        in (self: super:
+          body
+        );
     };
   };
 }
