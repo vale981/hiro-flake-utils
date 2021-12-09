db06c51475b0b8b03efac39b66ffa6fe64693b9e
Valentin Boettcher <hiro@protagon.space>
2021-12-09 12:02:57 +0100

    add poetry2nixWrapper

--- flake.nix
+++ flake.nix
@@ -1,9 +1,11 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    poetry2nix.url = "github:nix-community/poetry2nix";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }: {
     lib = rec {
       currentDefaultPackage = flake: system: flake.defaultPackage.${system};
       makeDefaultPackageOverrides =
@@ -19,6 +21,51 @@
         in (self: super:
           body
         ));
+
+      poetry2nixWrapper = pythonInputs: {name, poetryArgs, buildInputs}:
+        (flake-utils.lib.eachDefaultSystem (system:
+          let
+            overlay = nixpkgs.lib.composeManyExtensions [
+              poetry2nix.overlay
+
+              (final: prev:
+            let overrides = prev.poetry2nix.overrides.withDefaults
+              (makeDefaultPackageOverrides pythonInputs system);
+            in
+            {
+              ${name} = (prev.poetry2nix.mkPoetryApplication {
+                projectDir = ./.;
+                preferWheels = true;
+                overrides = overrides;
+              });
+
+              "${name}Shell" = (prev.poetry2nix.mkPoetryEnv {
+                projectDir = ./.;
+                overrides = overrides;
+                preferWheels = true;
+                editablePackageSources = {
+                  ${name} = ./${name};
+                };
+              });
+            })
+
+        ];
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ overlay ];
+          config.allowUnfree = true;
+        };
+      in
+      rec {
+        packages = {
+          ${name} = pkgs.${name};
+        };
+
+        defaultPackage = packages.${name};
+        devShell = pkgs."${name}Shell".env.overrideAttrs (oldAttrs: {
+          buildInputs = (buildInputs.pkgs);
+        });
+      }));
     };
   };
 }
